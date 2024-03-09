f242cf8561f17ba75bf3f876fdabe183e784b725
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2024-03-09 10:00:33 -0800

    added redux and fetch products using redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,8 @@ import {
 } from "react-router-dom";
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
+import { Provider } from "react-redux";
+import store from "./store.js";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import HomeScreen from "./screens/HomeScreen";
@@ -25,7 +27,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
