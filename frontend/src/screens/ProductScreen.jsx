65d49f58f6dee3f84d683740561ff2d37f00d040
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2024-03-11 04:02:34 -0700

    add message and loader component

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
+import Loader from "../components/Loader.jsx";
+import Message from "../components/Message.jsx";
 import { useGetProductDetailsQuery } from "../slices/productsApiSlice.js";
 
 const ProductScreen = () => {
@@ -20,9 +22,11 @@ const ProductScreen = () => {
         Go Back
       </Link>
       {isLoading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
-        <div>{error?.data?.message || error.error}</div>
+        <Message variant={danger}>
+          {error?.data?.message || error.error}
+        </Message>
       ) : (
         <Row>
           <Col md={5}>
